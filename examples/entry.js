23567495c7ca755b8db5c7cbd7d2235d0434a597
wangzheng <zheng.wang@fih-foxconn.com>
2021-10-15 16:59:44 +0800

    remove examples

--- examples/entry.js
+++
@@ -1,6 +0,0 @@
-import { name } from './package.json';
-function init() {
-  name && (document.title = name.toUpperCase());
-  document.querySelector('#app').innerHTML = 'THIS IS MY APP';
-}
-init();
